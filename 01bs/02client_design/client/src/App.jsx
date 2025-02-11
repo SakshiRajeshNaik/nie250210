3ab7c9194722f076c5079d1b22f4432efaf70ee4
SakshiRajeshNaik <sakshi345naik@gmail.com>
2025-02-11 14:54:56 +0530

    go lang full stack

--- 01bs/02client_design/client/src/App.jsx
+++ 01bs/02client_design/client/src/App.jsx
@@ -1,24 +1,22 @@
-
 import FlightList from "./flights/FlightList"
 import FlightCreate from "./flights/FlightCreate"
 import FlightEdit from "./flights/FlightEdit"
-import{ BrowserRouter, Route, Routes } from 'react-router-dom'
-function App() {
- 
+import Counter from "./Counter" 
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+function App() {//app is a functional component of UI
   return (
     <>
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path=''element={<FlightList/>}/>
-          <Route path='flights/list'element={<FlightList/>}/>
-          <Route path='flights/create'element={<FlightCreate/>}/>
-          <Route path='flights/edit'element={<FlightEdit/>}/>
-        </Routes>
-      
-      </BrowserRouter>
-    </div>
-     
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="" element={<FlightList/>}/>
+            <Route path="/flights/list" element={<FlightList/>}/>
+            <Route path="/flights/create" element={<FlightCreate/>}/>
+            <Route path="/flights/edit/:id" element={<FlightEdit/>}/>
+          </Routes>
+        </BrowserRouter>
+      </div>
+      <Counter/>
     </>
   )
 }
